ad7abb25f36150cab229029659f0e67d0694be8c
mbganesh <crazyganesh179@gmail.com>
2022-07-05 21:40:41 +0530

    version 1

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+const app = express()
+import moment from 'moment'
+import http from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
+
+app.use(cors())
+app.use(express.json())
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(`User ID : ${socket.id}`);
+    socket.on('send_msg', (data) => {
+        console.log(data);
+        let obj = {
+            time:moment().format('MMMM Do YYYY, h:mm:ss a'),
+            text:data.message
+        }
+        socket.broadcast.emit('receive_msg',obj)
+    })
+})
+
+app.get('/' , (req , res) => {
+    res.json('Server Works')
+})
+
+server.listen(3001)
